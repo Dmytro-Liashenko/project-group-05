fb4b1734206e702f361f3c942c0f8704f6b9485e
rilly <dimka4545452@gmail.com>
2025-07-29 10:30:07 +0300

    fixed button on tablet version

--- src/js/modal-three.js
+++ src/js/modal-three.js
@@ -1,21 +1,27 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-three-open]"),
-      closeModalBtn: document.querySelector("[data-modal-three-close]"),
-      modal: document.querySelector("[data-modal-three]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
+  const refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-three-open]"),
+    closeModalBtn: document.querySelector("[data-modal-three-close]"),
+    modal: document.querySelector("[data-modal-three]"),
+  };
 
-     refs.modal.addEventListener("click", (e) => {
+  // Навесить на все кнопки открытия
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
+
+  // Закрытие по кнопке
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  // Закрытие по клику на бэкдроп
+  refs.modal.addEventListener("click", (e) => {
     if (e.target === refs.modal) {
       toggleModal();
     }
   });
-    function toggleModal() {
-      refs.modal.classList.toggle("is-open");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
